88b4d251c655ed282d316031abdb4643b16381ba
kkata <14849535+kkata@users.noreply.github.com>
2022-07-28 10:49:58 +0900

    adding firebase emulator

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,5 +14,14 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+initializeApp(firebaseConfig);
+
+export const db = getFirestore();
+export const auth = getAuth();
+const storage = getStorage();
+
+if (process.env.NODE_ENV !== "production") {
+  connectFirestoreEmulator(db, "localhost", 8080);
+  connectAuthEmulator(auth, "http://localhost:9099");
+  connectStorageEmulator(storage, "localhost", 9199);
+}
