6d58f23e414eae1d0cd08f584e8ce1279fa29831
kkata <14849535+kkata@users.noreply.github.com>
2022-07-29 19:13:02 +0900

    alerts with react toastify

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
@@ -60,7 +61,7 @@ export const SignUp = () => {
 
       navigate("/");
     } catch (error) {
-      console.log(error);
+      toast.error("Something went wrong");
     }
   };
 
