bf21aebf9702d8365e39ef4e25812e955319e300
kkata <14849535+kkata@users.noreply.github.com>
2022-08-27 13:09:46 +0900

    fix useAuthStatus hook

--- src/hooks/useAuthStatus.tsx
+++ src/hooks/useAuthStatus.tsx
@@ -1,27 +1,22 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
-  const isMounted = useRef(true);
 
   useEffect(() => {
-    if (isMounted) {
-      const auth = getAuth();
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-          setLoggedIn(true);
-        }
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
 
-        setCheckingStatus(false);
-      });
-    }
+      setCheckingStatus(false);
+    });
 
-    return () => {
-      isMounted.current = false;
-    };
-  }, [isMounted]);
+    return unsubscribe;
+  }, []);
 
   return { loggedIn, checkingStatus };
 };
